1126e6722f3967534daeb05e257703e369fb2c23
Daler <dsarifkulo021@gmail.com>
2022-11-17 11:36:48 +0500

    fix

---
+++ index.js
@@ -0,0 +1,61 @@
+let quiz = {
+    id: 1,
+    question: "A candidate for the salary increase is",
+    translate: "Никакого повышения. Командный игрок — это хорошо, но компания больше ценит тех, кто достигает цели, развивается и получает позитивную обратную связь от клиентов.",
+    type: "radio",
+    options: [
+        {
+            id: 1,
+            option: "a team player"
+        },
+        {
+            id: 2,
+            option: "a goal-achiever who gives a positive experience to clients"
+        },
+        {
+            id: 3,
+            option: "an employee who fulfilled sales plan"
+        },
+    ]
+}
+let quiz_type_two = {
+    id: 2,
+    question: "A candidate for the salary increase is",
+    translate: "Никакого повышения. Командный игрок — это хорошо, но компания больше ценит тех, кто достигает цели, развивается и получает позитивную обратную связь от клиентов.",
+    type: "radio_audio",
+    diolgue: "dialogue_first.mp3",
+    options: [
+        {
+            id: 1,
+            option: "a team player"
+        },
+        {
+            id: 2,
+            option: "a goal-achiever who gives a positive experience to clients"
+        },
+        {
+            id: 3,
+            option: "an employee who fulfilled sales plan"
+        },
+    ]
+}
+let quiz_type_three = {
+    id: 3,
+    question: "If you match these criteria, contact your manager because you qualify for a salary <@select@>",
+    translate: "Никакого повышения. Командный игрок — это хорошо, но компания больше ценит тех, кто достигает цели, развивается и получает позитивную обратную связь от клиентов.",
+    type: "select",
+    options: [
+        {
+            id: 1,
+            option: "a team player"
+        },
+        {
+            id: 2,
+            option: "a goal-achiever who gives a positive experience to clients"
+        },
+        {
+            id: 3,
+            option: "an employee who fulfilled sales plan"
+        },
+    ]
+}
