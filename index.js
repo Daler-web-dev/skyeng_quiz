06fa78e2dec1aa67348d4d6f6a3e1d2bc32a35e5
Daler <dsarifkulo021@gmail.com>
2022-11-19 10:38:22 +0500

    alisher's changings

--- index.js
+++ index.js
@@ -18,6 +18,30 @@ let quiz = {
         },
     ]
 }
+
+let radioQST = [
+    {
+        id: 1,
+        question: "A candidate for the salary increase is",
+        translate: "Никакого повышения. Командный игрок — это хорошо, но компания больше ценит тех, кто достигает цели, развивается и получает позитивную обратную связь от клиентов.",
+        type: "radio",
+        options: [
+            {
+                id: 1,
+                option: "a team player"
+            },
+            {
+                id: 2,
+                option: "a goal-achiever who gives a positive experience to clients"
+            },
+            {
+                id: 3,
+                option: "an employee who fulfilled sales plan"
+            },
+        ]
+    },
+]
+
 let quiz_type_two = {
     id: 2,
     question: "A candidate for the salary increase is",
