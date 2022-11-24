052e0e1c326ccbdc0e0faa1bab480cfddc866994
Daler <dsarifkulo021@gmail.com>
2022-11-24 12:12:58 +0500

    audio questions was done

--- modules/quiz.js
+++ modules/quiz.js
@@ -121,13 +121,14 @@ const renderRadioQuestion = (question, user) => {
 }
 
 
-Promise.all([getQsts('/select_questions'), getQsts('/radio_questions'), getQsts('/userScore')])
+Promise.all([getQsts('/select_questions'), getQsts('/radio_questions'), getQsts('/auido_questions'), getQsts('/userScore')])
     .then(res => {
         let counter = 0
         let selectCounter = 1
         let radioCounter = 0
+        let audioCounter = 0
 
-        let userScore = res[2]
+        let userScore = res[3]
 
 
         if (counter % 2 === 0) {
@@ -140,14 +141,70 @@ Promise.all([getQsts('/select_questions'), getQsts('/radio_questions'), getQsts(
 
         goOnBtn.onclick = () => {
             counter++
-            if (counter % 2 === 0) {
-                let questionTypeOne = res[0][selectCounter]
-                renderSelectQuestion(questionTypeOne, userScore)
-                selectCounter++
+            if (counter >= 6) {
+                // audio
+                let arr = res[2]
+                const renderAudioQuestion = (question, user) => {
+                    radioCont.innerHTML = ""
+                    p.innerHTML = ""
+                    let h1 = document.querySelector('h1')
+
+                    let audio = document.createElement('audio')
+
+                    h1.innerHTML = question.question
+                    audio.src = `../audio${question.diolgue}`
+                    audio.setAttribute("controls", "controls");
+
+                    radioCont.before(audio)
+
+                    for (let item of question.options) {
+                        let div = document.createElement('div')
+                        let span = document.createElement('span')
+
+                        div.classList.add('radio')
+                        span.innerHTML = item.option
+                        div.setAttribute('id', item.id)
+
+                        div.append(span)
+                        radioCont.append(div)
+
+                        if (item.isTrue === true) trueID = question.options.indexOf(item)
+
+                        div.onclick = () => {
+                            if (item.isTrue) {
+                                div.classList.add('radio-active-valid')
+                                div.removeAttribute('onclick')
+                                setAnswer('/userScore', {
+                                        trueAnswers: user.trueAnswers + 1,
+                                    })
+                                    .then(() => showBtn())
+                                    audioCounter++
+                            } else {
+                                div.classList.add('radio-active-invalid')
+                                console.log(question.options[trueID]);
+                                document.querySelectorAll('.radio')[trueID].classList.add('radio-active-valid')
+                                div.removeAttribute('onclick')
+                                setAnswer('/userScore', {
+                                        falseAnswers: user.falseAnswers + 1,
+                                        falseAnswersArr: [...user.falseAnswersArr, question]
+                                    })
+                                    .then(() => showBtn())
+                                    audioCounter++
+                            }
+                        }
+                    }
+                }
+                renderAudioQuestion(arr[audioCounter], userScore)
             } else {
-                let questionTypeTwo = res[1][radioCounter]
-                renderRadioQuestion(questionTypeTwo, userScore)
-                radioCounter++
+                if (counter % 2 === 0) {
+                    let questionTypeOne = res[0][selectCounter]
+                    renderSelectQuestion(questionTypeOne, userScore)
+                    selectCounter++
+                } else {
+                    let questionTypeTwo = res[1][radioCounter]
+                    renderRadioQuestion(questionTypeTwo, userScore)
+                    radioCounter++
+                }
             }
             goOnBtn.classList.remove('purple-active')
         }
